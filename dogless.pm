d3445c702ba85767a9940e74cda93b3f5b9be7fc
Adam Curtis <acurtis@spsu.edu>
2012-07-28 01:10:37 -0400

    first commit

---
+++ dogless.pm
@@ -0,0 +1,94 @@
+#!/usr/bin/perl
+package Dogless;
+use strict;
+use warnings; no warnings 'uninitialized';
+use v5.10;
+use Getopt::Long;
+
+sub help {
+<<__HELP__
+Usage: $0 [options] [files]
+
+Interprets dogless programs passed as command line arguments, or from stdin if no files are specified.
+
+Options:
+
+  -d --debug        Enters debug mode, the source code is 
+                    printed for each transformation.
+  -h --help         This help text
+
+  -i --interval     specifies a decimal interval in seconds to
+                    wait between commands
+__HELP__
+}
+
+#command line options
+my ($help, $debug_mode, $interval);
+
+my $any_command;
+$any_command = qr/(<|>)(??{$any_command})|\\.|s..|.|$/;
+
+sub run_cmd {
+    my ($_, $pre, $post, $s, $total) = @_;
+    my ($x, $y, $c);
+    if(($c) = /^<(.*)$/p) {
+        $pre =~ /\||$/p;
+        $pre = run_cmd($c, ${^PREMATCH}, ${^POSTMATCH}, ${^MATCH}, $pre);
+        return "$pre$s$post";
+    }
+    elsif (($c) = /^>(.*)$/p) {
+        $post =~ /\||$/p;
+        $post = run_cmd($c, ${^PREMATCH}, ${^POSTMATCH}, ${^MATCH}, $post);
+        return "$pre$s$post";
+    }
+    elsif(($x, $y) = /^s(.)(.)$/sp) {
+        $_ = "$pre$s$post";
+        s/\Q$x\E/$y/;
+        return $_;
+    }
+    elsif (/^"$/p) {
+        $post =~ /^[^"]*("|$)/p;
+        return "$pre$s${^POSTMATCH}";
+    }
+    elsif (/^\?$/p) {
+        return reverse "$pre$s$post";
+    }
+    elsif (/^\^$/) {
+        return "$post$s$pre";
+    }
+    elsif (/^\~$/p) {
+        return "$pre$s$post" . $total;
+    }
+    elsif (/^!$/) {
+        return '';
+    }
+    elsif (/^\|$/p) {
+        return "$pre$s$post";
+    }
+    elsif (($c) = /^\\?(.)$/sp){
+        return "$pre$c$s$post";
+    }
+    else {
+        return "$pre$post";
+    }
+}
+
+sub interpret {
+    my ($_) = @_;
+    while (/(\|)($any_command)/) {
+        say if $debug_mode;
+        sleep $interval if defined $interval;
+        $_ = run_cmd($2, ${^PREMATCH}, ${^POSTMATCH}, '|', $_);
+    }
+    return $_;
+}
+
+
+unless (caller) {
+    GetOptions ("help|h" => \$help,
+                "debug|d" => \$debug_mode,
+                "interval|i=f" => \$interval,
+               ) or die "Invalid options.";
+    say help() and exit if $help;
+    print interpret (join '', <>);
+}
